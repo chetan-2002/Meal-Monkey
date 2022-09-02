a2a292c422b9d3d1b06bc30326e37ff5b2cd202c
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-09-02 13:20:37 +0530

    added Cart Modal and Single Cart Item Component

---
+++ src/modules/cart/components/CartModal.js
@@ -0,0 +1,140 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { roundTo } from "round-to";
+import React, { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import { CartState } from "../../../Context/CartProvider";
+import SingleCartItem from "./SingleCartItem";
+import { useNavigate } from "react-router-dom";
+
+const CartModal = ({ children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [disabled, setDisabled] = React.useState(true);
+  const navigate = useNavigate();
+  const { user, cart } = CartState();
+  // const cart = JSON.parse(localStorage.getItem("cart"));
+  useEffect(() => {
+    if (cart?.cartItems?.length > 0 && user) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [user, cart]);
+  return (
+    <>
+      <span onClick={onOpen}>{children}</span>
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered size={"xl"}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <Text fontSize={"2xl"} fontWeight={"medium"}>
+              Your cart:
+            </Text>
+          </ModalHeader>
+          <ModalBody
+            maxH={"300px"}
+            overflowY={"scroll"}
+            css={{
+              scrollbarWidth: "none",
+              "::-webkit-scrollbar": { display: "none" },
+              WebkitOverflowScrolling: "touch",
+            }}
+          >
+            <Box
+              overflowY={"auto"}
+              display={"flex"}
+              flexDir={"column"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              {user && cart?.cartItems?.length ? (
+                <>
+                  {cart?.cartItems?.map((item, index) => (
+                    <SingleCartItem key={index} item={item} />
+                  ))}
+                  {<Divider orientation="horizontal" color={"black"} />}
+                  {
+                    <>
+                      <Box
+                        display={"flex"}
+                        justifyContent={"space-between"}
+                        minWidth={"full"}
+                        padding={4}
+                      >
+                        <Box fontSize={"xl"}>Total Amount </Box>
+                        <Box fontSize={"xl"}>₹ {roundTo(cart?.total, 2)}</Box>
+                      </Box>
+                      <Box
+                        display={"flex"}
+                        justifyContent={"space-between"}
+                        minWidth={"full"}
+                        padding={4}
+                      >
+                        <Box fontSize={"xl"}>GST (18%) </Box>
+                        <Box fontSize={"xl"}>
+                          ₹ {roundTo(cart?.total * 0.18, 2)}
+                        </Box>
+                      </Box>
+                      <Box
+                        display={"flex"}
+                        justifyContent={"space-between"}
+                        minWidth={"full"}
+                        padding={4}
+                      >
+                        <Box fontWeight={"bold"} fontSize={"xl"}>
+                          Total Amount after Taxes{" "}
+                        </Box>
+                        <Box fontWeight={"bold"} fontSize={"xl"}>
+                          ₹ {roundTo(cart?.total + 0.18 * cart?.total, 0)}
+                        </Box>
+                      </Box>
+                    </>
+                  }
+                </>
+              ) : (
+                <>
+                  <FontAwesomeIcon icon={faCartPlus} size={"4x"} />
+                  <Text fontSize={"medium"} p={4}>
+                    No items in cart yet!
+                  </Text>
+                </>
+              )}
+            </Box>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Continue Shopping
+            </Button>
+            <Button
+              colorScheme={"whatsapp"}
+              variant="outline"
+              disabled={disabled}
+              onClick={() => {
+                onClose();
+                navigate("/checkout");
+              }}
+            >
+              Checkout
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default CartModal;
---
+++ src/modules/cart/components/SingleCartItem.js
@@ -0,0 +1,121 @@
+import {
+  Box,
+  IconButton,
+  Image,
+  Spinner,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import { CartState } from "../../../Context/CartProvider";
+
+const SingleCartItem = ({ item }) => {
+  const { setCart, user } = CartState();
+  const toast = useToast();
+  const [loading, setLoading] = React.useState(false);
+  const updateProduct = async (productId, operation) => {
+    setLoading(true);
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${
+          JSON.parse(localStorage.getItem("userInfo")).token
+        }`,
+      },
+    };
+    axios
+      .post(
+        "http://localhost:5000/api/cart/updateCartItems",
+        { productId, operation, userId: user._id },
+        config
+      )
+      .then((res) => {
+        setCart(res.data);
+        toast({
+          title: `Cart updated successfully`,
+          description:
+            operation === "inc"
+              ? `A ${item.product.name} was added`
+              : `A ${item.product.name} is removed`,
+          status: operation === "inc" ? "success" : "warning",
+          duration: 3000,
+          isClosable: true,
+        });
+        setLoading(false);
+      })
+      .catch((err) => {
+        toast({
+          title: "Error",
+          description: "Something went wrong",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+        setLoading(false);
+      });
+  };
+  return (
+    <Box
+      borderRadius={5}
+      display={"flex"}
+      width={"100%"}
+      alignItems={"center"}
+      justifyContent={"space-between"}
+      p={{ base: "2", md: "4" }}
+    >
+      <Image
+        src={item.product.image}
+        alt={item.product.name}
+        width={{ base: "4rem", md: "6rem" }}
+        height={{ base: "4rem", md: "6rem" }}
+        borderRadius={"8px"}
+        display={{ base: "none", md: "block" }}
+      />
+      <Box flexBasis={{ basis: "25%" }}>
+        <Text
+          padding={3}
+          fontSize={{ base: "xl", md: "2xl" }}
+          fontWeight={{ base: "medium", md: "bold" }}
+          width={{ base: "100%" }}
+        >
+          {item.product.name.length > 10
+            ? item.product.name.substring(0, 7) + "..."
+            : item.product.name}
+        </Text>
+      </Box>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Box p={{ base: "1", md: 3 }} display={"flex"} alignItems={"center"}>
+          <IconButton
+            icon={<AiOutlinePlus />}
+            borderRadius={"full"}
+            onClick={() => updateProduct(item.product._id, "inc")}
+          ></IconButton>
+          <Text p={{ base: "2", md: "4" }} fontWeight={"bold"}>
+            {item.qty}
+          </Text>
+          <IconButton
+            icon={<AiOutlineMinus />}
+            borderRadius={"full"}
+            onClick={() => updateProduct(item.product._id, "dec")}
+          ></IconButton>
+        </Box>
+      )}
+
+      <Box display={"flex"} justifyContent={"flex-end"}>
+        <Text
+          fontSize={{ base: "md", md: "xl" }}
+          fontWeight={{ base: "medium", md: "bold" }}
+          p={{ base: "1", md: "4" }}
+        >
+          ₹ {item.product.price * item.qty}
+        </Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default SingleCartItem;
