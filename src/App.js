4d7eab7b0ef1e277163b250356257fb6d5945aca
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-09-02 13:06:25 +0530

    added configuration files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import AppRoutes from "./config/routes/AppRoutes";
+import { Box } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+// import nativeOperations from "./shared/services/native";
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    localStorage.setItem("chakra-ui-color-mode", "light");
+    navigate("/menu");
+    //eslint-disable-next-line
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box bgColor={"gray.100"}>
+      <AppRoutes />
+    </Box>
   );
 }
 
