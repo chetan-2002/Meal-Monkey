1c896fdee8c48704b949a3b2a15ed07caca140e9
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-09-02 13:23:35 +0530

    added page for Checkout

---
+++ src/modules/order/pages/Checkout.js
@@ -0,0 +1,29 @@
+import { Box } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import OrderSummary from "../components/OrderSummary";
+import ShippingInformation from "../components/ShippingInformation";
+import { useNavigate } from "react-router-dom";
+import { CartState } from "../../../Context/CartProvider";
+const Checkout = () => {
+  const { orderPlaced, setOrderPlaced } = CartState();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (orderPlaced) {
+      setOrderPlaced(false);
+      navigate("/menu");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <Box
+      backgroundColor={"white"}
+      display={"flex"}
+      flexDirection={{ base: "column", md: "row" }}
+    >
+      <ShippingInformation />
+      <OrderSummary />
+    </Box>
+  );
+};
+
+export default Checkout;
