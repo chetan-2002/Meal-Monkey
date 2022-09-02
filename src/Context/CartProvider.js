da352904d5d1267b18e28b4dc75ff626e6152c1f
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-09-02 13:08:52 +0530

    added React Api Context to the application

---
+++ src/Context/CartProvider.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import React, { createContext, useEffect } from "react";
+
+const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = React.useState({});
+  const [user, setUser] = React.useState(null);
+  const [shippingInfo, setShippingInfo] = React.useState({});
+  const [orderPlaced, setOrderPlaced] = React.useState(false);
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    if (userInfo) {
+      setUser(userInfo);
+      const configuration = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem("userInfo")).token
+          }`,
+        },
+      };
+      axios
+        .post(
+          "http://localhost:5000/api/cart/getCartItems",
+          { userId: userInfo._id },
+          configuration
+        )
+        .then((res) => {
+          setCart(res.data);
+          // localStorage.setItem("cart", JSON.stringify(res.data));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    // eslint-disable-next-line
+  }, []);
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        setCart,
+        user,
+        setUser,
+        shippingInfo,
+        setShippingInfo,
+        orderPlaced,
+        setOrderPlaced,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const CartState = () => {
+  return React.useContext(CartContext);
+};
+export default CartProvider;
