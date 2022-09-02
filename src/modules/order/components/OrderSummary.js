714b1b0fe5b2730ebb34e81d07da6fbea1329fea
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-09-02 13:22:44 +0530

    Created components for shipping Info and order summary

---
+++ src/modules/order/components/OrderSummary.js
@@ -0,0 +1,199 @@
+import { Box, Button, Divider, Text, useToast } from "@chakra-ui/react";
+import axios from "axios";
+import React from "react";
+import { roundTo } from "round-to";
+import { CartState } from "../../../Context/CartProvider";
+import SingleOrderItem from "./SingleOrderItem";
+import { useNavigate } from "react-router-dom";
+
+const OrderSummary = () => {
+  const { cart, shippingInfo, user, setOrderPlaced } = CartState();
+  const [loading, setLoading] = React.useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
+  const handleOrder = async () => {
+    // console.log("Order Placed");
+    setLoading(true);
+    if (
+      !shippingInfo.address ||
+      !shippingInfo.paymentType ||
+      !shippingInfo.phoneNo
+    ) {
+      toast({
+        title: "Please Enter all the fields",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      setLoading(false);
+    } else {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem("userInfo")).token
+          }`,
+        },
+      };
+      await axios
+        .post("http://localhost:5000/api/order/makeOrder", shippingInfo, config)
+        .then((res) => {
+          toast({
+            title: "Order Placed Successfully",
+            status: "success",
+            duration: 3000,
+            isClosable: true,
+          });
+          axios.post(
+            "http://localhost:5000/api/sendmail",
+            {
+              email: user.email,
+              html: `<h3>Hey ${user.name}, </h3>
+              Thank you for shopping with us. Your order has been placed successfully.
+              <br>
+              <h3>Order Details</h3>
+              <table border="1" cellpadding="10" cellspacing="0">
+              <tr>
+              <th>S.No</th>
+              <th>Product Name</th>
+              <th>Product Price</th>
+              <th>Product Quantity</th>
+              <th>Product Total</th>
+              </tr>
+              ${cart.cartItems
+                .map(
+                  (item) =>
+                    `<tr>
+                <td>${cart.cartItems.indexOf(item) + 1}</td>
+                <td>${item.product.name}</td>
+                <td>₹ ${item.product.price}</td>
+                <td>${item.qty}</td>
+                <td>₹ ${item.product.price * item.qty}</td>
+                </tr>`
+                )
+                .join("")}
+              </table>
+              <br>
+              <h3>Shipping Details</h3>
+              <table border="1" cellpadding="10" cellspacing="0">
+              <tr>
+              <th>Address</th>
+              <th>Phone No</th>
+              <th>Payment Type</th>
+              <th>Total Amount Payable (After Taxes)</th>
+              </tr>
+              <tr>
+              <td>${shippingInfo.address}</td>
+              <td>${shippingInfo.phoneNo}</td>
+              <td>${shippingInfo.paymentType}</td>
+              <td>₹ ${roundTo(cart.total + 0.18 * cart.total, 0)}</td>
+              </tr>
+              </table>
+              <br>
+              <h3>Order Placed On</h3>
+              <table border="1" cellpadding="10" cellspacing="0">
+              <tr>
+              <th>${new Date().toLocaleString()}</th>
+              </tr>
+              </table>
+              <h3>Regards,</h3>
+              <h3>Team Meal Monkey</h3>
+              `,
+            },
+            config
+          );
+          setOrderPlaced(true);
+          navigate("/orders");
+          setLoading(false);
+        })
+        .catch((err) => {
+          toast({
+            title: "Something went wrong",
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+          });
+          setLoading(false);
+        });
+    }
+  };
+
+  return (
+    <Box
+      display={"flex"}
+      flexDir={"column"}
+      backgroundColor={"gray.50"}
+      width={{ base: "100%", md: "36%" }}
+    >
+      <Box p={6}>
+        {
+          <Text p={4} fontSize={"xl"} fontWeight={"bold"}>
+            Order Summary:
+          </Text>
+        }
+
+        {cart.cartItems?.map((item) => (
+          <SingleOrderItem key={item._id} item={item} />
+        ))}
+        {<Divider orientation="horizontal" borderColor={"gray.400"} />}
+        {
+          <Box
+            display={"flex"}
+            flexDir={"column"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <Box
+              display={"flex"}
+              justifyContent={"space-between"}
+              minWidth={"full"}
+              padding={4}
+            >
+              <Box fontSize={"xl"}>Total Amount </Box>
+              <Box fontSize={"xl"}>
+                {user ? `₹ ${roundTo(cart?.total, 2)}` : <></>}
+              </Box>
+            </Box>
+            <Box
+              display={"flex"}
+              justifyContent={"space-between"}
+              minWidth={"full"}
+              padding={4}
+            >
+              <Box fontSize={"xl"}>GST (18%) </Box>
+              <Box fontSize={"xl"}>₹ {roundTo(cart?.total * 0.18, 2)}</Box>
+            </Box>
+            <Box
+              display={"flex"}
+              justifyContent={"space-between"}
+              minWidth={"full"}
+              padding={4}
+            >
+              <Box fontWeight={"medium"} fontSize={"xl"}>
+                Total Amount after Taxes{" "}
+              </Box>
+              <Box fontWeight={"medium"} fontSize={"xl"}>
+                ₹ {roundTo(cart?.total + 0.18 * cart?.total, 0)}
+              </Box>
+            </Box>
+          </Box>
+        }
+        {
+          <Box p={2}>
+            <Button
+              variant={"solid"}
+              colorScheme={"blue"}
+              width={"full"}
+              onClick={handleOrder}
+              isLoading={loading}
+            >
+              Place Order
+            </Button>
+          </Box>
+        }
+      </Box>
+    </Box>
+  );
+};
+
+export default OrderSummary;
---
+++ src/modules/order/components/ShippingInformation.js
@@ -0,0 +1,199 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  HStack,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Radio,
+  RadioGroup,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { MdLocationOn } from "react-icons/md";
+import { roundTo } from "round-to";
+import { CartState } from "../../../Context/CartProvider";
+// import { CartState } from "../../../Context/CartProvider";
+import { useNavigate } from "react-router-dom";
+const ShippingInformation = () => {
+  const { cart, user, setShippingInfo } = CartState();
+  const navigate = useNavigate();
+  const cartId = cart._id;
+  const [City, setCity] = React.useState("");
+  const toast = useToast();
+  const [shippingInformation, setShippingInformation] = React.useState({
+    userId: user._id,
+    cart: cartId,
+    address: "",
+    paymentType: "COD",
+    total: roundTo(cart?.total + 0.18 * cart?.total, 0),
+    phoneNo: "",
+  });
+  const [loading, setLoading] = React.useState(false);
+  const getUserLocation = async () => {
+    if (!navigator.geolocation) {
+      toast({
+        title: "Geolocation is not supported by your browser",
+        description: "Please enter your address manually",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+    setLoading(true);
+    let lat, lon;
+    navigator.geolocation.getCurrentPosition((position) => {
+      lat = position.coords.latitude.toFixed(7);
+      lon = position.coords.longitude.toFixed(7);
+      axios
+        .get(
+          `http://api.positionstack.com/v1/reverse?access_key=f2240e35c5b3530ce72d0da45273d907&query=${lat},${lon}`
+        )
+        .then((res) => {
+          setShippingInformation({
+            ...shippingInformation,
+            address:
+              res.data.data[0].name +
+              ", " +
+              res.data.data[0].neighbourhood +
+              ", " +
+              res.data.data[0].county +
+              ", " +
+              res.data.data[0].region,
+          });
+          setCity(res.data.data[0].region);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    });
+  };
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    if (!userInfo) {
+      navigate("/menu");
+    }
+    setShippingInfo(shippingInformation);
+    // eslint-disable-next-line
+  }, [shippingInformation]);
+  return (
+    <Box width={{ base: "100%", md: "64%" }} p={6}>
+      <Text fontSize={"xl"} fontWeight={"bold"}>
+        Shipping Information:
+      </Text>
+      <Box>
+        <FormControl py={4} isReadOnly>
+          <FormLabel py={2}>Full Name</FormLabel>
+          <Input value={user?.name} placeholder="Enter your full Name"></Input>
+        </FormControl>
+        <FormControl py={6} isRequired>
+          <FormLabel py={2}>Street Address</FormLabel>
+          <InputGroup>
+            <Input
+              placeholder="Enter the Street Address"
+              value={shippingInformation.address}
+              onChange={(e) =>
+                setShippingInformation({
+                  ...shippingInformation,
+                  address: e.target.value,
+                })
+              }
+            ></Input>
+            <IconButton
+              isLoading={loading}
+              ml={"2"}
+              icon={<MdLocationOn />}
+              onClick={getUserLocation}
+              variant={"outline"}
+            ></IconButton>
+          </InputGroup>
+        </FormControl>
+        <Box display={"flex"} alignItems={"center"}>
+          <FormControl
+            py={4}
+            isRequired
+            width={{ base: "100%", md: "33%" }}
+            pr={4}
+          >
+            <FormLabel py={2}>Zip Code</FormLabel>
+            <Input type={"number"} placeholder="Enter Zip Code"></Input>
+          </FormControl>
+          <FormControl py={6} isRequired>
+            <FormLabel py={2}>City</FormLabel>
+            <Input
+              placeholder="Enter City Name"
+              value={City}
+              onChange={(e) => {
+                setCity(e.target.value);
+              }}
+            ></Input>
+          </FormControl>
+        </Box>
+        <FormControl py={6} isReadOnly>
+          <FormLabel py={2}>Email Address</FormLabel>
+          <Input
+            type={"email"}
+            value={user?.email}
+            placeholder="Enter Your Email Address"
+          ></Input>
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel py={2}>Phone Number</FormLabel>
+          <InputGroup>
+            <InputLeftAddon children="+91" />
+            <Input
+              type="tel"
+              placeholder="Phone number"
+              value={shippingInformation.phoneNo}
+              onChange={(e) => {
+                setShippingInformation({
+                  ...shippingInformation,
+                  phoneNo: e.target.value,
+                });
+              }}
+            />
+          </InputGroup>
+        </FormControl>
+      </Box>
+      <Text fontSize={"xl"} fontWeight={"bold"} pt={12} pb={3}>
+        Payment Information:
+      </Text>
+      <Box py={4}>
+        <FormControl as="fieldset">
+          <RadioGroup
+            // defaultValue="COD"
+            value={shippingInformation.paymentType}
+            onChange={(e) => {
+              setShippingInformation({
+                ...shippingInformation,
+                paymentType: e,
+              });
+            }}
+          >
+            <HStack
+              spacing="24px"
+              display={"flex"}
+              justifyContent={"space-between"}
+            >
+              <Radio value="COD">Cash on Delivery</Radio>
+              <Radio value="Card">Credit/Debit Card</Radio>
+              {/* <Radio value="UPI">UPI</Radio> */}
+            </HStack>
+          </RadioGroup>
+        </FormControl>
+        <Button display={"none"}> HEllo</Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default ShippingInformation;
